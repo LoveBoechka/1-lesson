1db39beed94ddb7931e558edc64e221edbfa76f5
LoveBoechka <166940170+LoveBoechka@users.noreply.github.com>
2025-09-08 15:25:15 +0300

    Add files via upload

---
+++ 1_3.java
@@ -0,0 +1,29 @@
+import java.util.ArrayList;
+public class Program {
+    public static void main(String args) {
+        ArrayList<String> people = new ArrayList<String>();
+        // добавим в список ряд элементов
+        people.add("Tom");
+        people.add("Алиса");
+        people.add("Kate");
+        people.add("Sam");
+        people.add(1, "Bob"); // добавляем элемент по индексу 1
+        System.out.println(people.get(1)); // получаем 2-й объект
+        people.set(1, "Robert"); // установка нового значения для 2-го объекта
+        System.out.printf("ArrayList has %d elements \n", people.size());
+        for (String person : people) {
+            System.out.println(person);
+        }
+        // проверяем наличие элемента
+        if (people.contains("Tom")) {
+            System.out.println("ArrayList contains Tom");
+        }
+        // удалим несколько объектов
+        people.remove("Robert"); // удаление конкретного элемента
+        people.remove(0); // удаление по индексу
+        Object peopleArray = people.toArray();
+        for (Object person : peopleArray) {
+            System.out.println(person);
+        }
+    }
+}
